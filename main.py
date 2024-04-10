54543cf4be05583ace441c0ccce4a1d9e464f31b
Rostislav <fadenougat@gmail.com>
2024-04-10 20:22:21 +0300

    0.2.1
    Минимальные изменения:
    авторизация (не работает)
    косметика
    выгрузка событий

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, Blueprint, request, flash, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
+from flask_login import UserMixin
 
 from manager_db import EventsDatabase, UserManager, USERS_DB
 from competition_results import ManageResults
@@ -7,7 +9,7 @@ from competition_results import ManageResults
 from time import sleep
 
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 
 app = Flask(__name__)
@@ -15,8 +17,65 @@ app.secret_key = "we9jh89g8&GVE"
 
 admin_bp = Blueprint('admin', __name__)
 
+login_manager = LoginManager(app)
+
+users = {'admin': {'password': 'admin_password'}}
+
+
+# Класс пользователя
+# Пример модели пользователя
+class User(UserMixin):
+    def __init__(self, user_id, username, password_hash, is_admin=False):
+        self.id = user_id
+        self.username = username
+        self.password_hash = password_hash
+        self.is_admin = is_admin
+
+users = [
+    User(1, 'admin', 'admin_password_hash', is_admin=True),
+    User(2, 'user', 'user_password_hash')
+]
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    for user in users:
+        if user.id == user_id:
+            return user
+    return None
+
+
+def is_admin(username, password):
+    for user in users:
+        if user.username == username and user.password_hash == password and user.is_admin:
+            return True
+    return False
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if is_admin(username, password):
+            user = next((user for user in users if user.username == username), None)
+            login_user(user)
+            return redirect(url_for('admin'))
+        else:
+            flash('Неправильное имя пользователя или пароль', 'error')
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Вы успешно вышли из системы', 'success')
+    return redirect(url_for('login'))
+
 
 @app.route('/')
+@app.route('/index')
 def index():
     events_database = EventsDatabase(USERS_DB)
     all_events = events_database.view_events()
@@ -26,17 +85,22 @@ def index():
         users_manager = UserManager(USERS_DB)
         for event in all_events:
             all_users = users_manager.load_all_users(event[1])
-
     except Exception as e:
         print(e)
         pass
 
-    return render_template('index.html', users=all_users)
+    # Логика обработки рангов
+    print(all_events)
+    return render_template('index.html', users=all_users, events=all_events)
 
 
-@admin_bp.route('/admin')
 @app.route('/admin', methods=['GET', 'POST'])
+@login_required
 def admin():
+    if not current_user.is_admin:
+        flash('Недостаточно привилегий для доступа к этой странице', 'error')
+        return redirect(url_for('/'))
+
     events_database = EventsDatabase(USERS_DB)
     all_events = events_database.view_events()
 
@@ -47,7 +111,6 @@ def admin():
             all_users = users_manager.load_all_users(event[1])
 
     except Exception as e:
-        # Если возникает ошибка при чтении из базы данных, обработаем ее здесь
         flash(f'Ошибка чтения данных из БД :(\n\n{e}', 'flash-error')
 
     if request.method == 'POST':
